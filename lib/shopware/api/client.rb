519832458df615985c5653fd0b69f211a2350c6b
Maik Kempe <mkempe@nrolla.com>
2014-05-23 15:28:56 +0200

    [✔] Fixed “create category”. [➠] Ruby codestyle, damn you javascript.

--- lib/shopware/api/client.rb
+++ lib/shopware/api/client.rb
@@ -1,22 +1,55 @@
+require 'httparty'
+
 module Shopware
   module API
     class Client
-      require 'httparty'
       include HTTParty
 
       headers 'Content-Type' => 'application/json', 'charset' => 'utf-8'
 
       format :json
 
+      query_string_normalizer proc { |query| query.to_json }
+
       def initialize(options = {})
         self.class.base_uri options['base_uri']
         self.class.digest_auth options['username'], options['key']
       end
 
-      def categories
+      def get_categories
         response = self.class.get '/categories'
 
-        response['success'] ? response['data'] : []
+        response['data']
+      end
+
+      def get_category(id)
+        response = self.class.get "/categories/#{id}"
+
+        response['data']
+      end
+
+      def find_category_by_name(name)
+        response = self.class.get '/categories'
+
+        if response['success']
+          categories = response['data']
+
+          categories.each do |category|
+            return category if category['name'] == name
+          end
+        end
+
+        nil
+      end
+
+      def create_category(properties)
+        response = self.class.post '/categories', body: properties
+
+        response['data']
+      end
+
+      def delete_category(id)
+        self.class.delete "/categories/#{id}"
       end
     end
   end
--- lib/shopware/cli/runner.rb
+++ lib/shopware/cli/runner.rb
@@ -21,39 +21,48 @@ module Shopware
       method_options verbose: false
       def import(file)
         if File.exist? file
-          info "Processing `#{File.basename file}`..."
+          # info "Processing `#{File.basename file}`..."
 
-          col_sep    = options.import['column_separator'] || '|'
-          quote_char = options.import['quote_character'] || '"'
+          # col_sep    = options.import['column_separator'] || '|'
+          # quote_char = options.import['quote_character'] || '"'
 
-          data = CSV.read file, col_sep: col_sep , quote_char: quote_char
+          # data = CSV.read file, col_sep: col_sep , quote_char: quote_char
 
-          data.each_with_index do |row, i|
-            category                = row[0]
-            subcategory             = row[1]
-            subcategory_description = row[2]
-            subsubcategory          = row[3]
-            brand                   = row[4]
-            number                  = row[5]
-            name                    = row[6]
-            content                 = row[7]
-            content_unit            = row[8]
-            order_number            = row[9]
-            description             = row[10]
-            code                    = row[11]
-            car_manufacturer        = row[12]
-            car_id                  = row[13]
-            property                = row[14]
-            test_method             = row[15]
-            property_value          = row[16]
-            property_unit           = row[17]
-            image_small             = row[18]
-            image_big               = row[19]
+          # data.each_with_index do |row, i|
+          #   category                = row[0]
+          #   subcategory             = row[1]
+          #   subcategory_description = row[2]
+          #   subsubcategory          = row[3]
+          #   brand                   = row[4]
+          #   number                  = row[5]
+          #   name                    = row[6]
+          #   content                 = row[7]
+          #   content_unit            = row[8]
+          #   order_number            = row[9]
+          #   description             = row[10]
+          #   code                    = row[11]
+          #   car_manufacturer        = row[12]
+          #   car_id                  = row[13]
+          #   property                = row[14]
+          #   test_method             = row[15]
+          #   property_value          = row[16]
+          #   property_unit           = row[17]
+          #   image_small             = row[18]
+          #   image_big               = row[19]
 
-            info "Category: #{category}"
-            info "Subcategory: #{subcategory}"
-            info "Subsubcategory: #{subsubcategory}"
-          end
+          #   info "Category: #{category}"
+          #   info "Subcategory: #{subcategory}"
+          #   info "Subsubcategory: #{subsubcategory}"
+          # end
+
+          client = API::Client.new options.api
+
+          category = client.create_category({
+            name: 'Foo',
+            parentId: '5'
+          })
+
+          pp category
         else
           error "File: `#{file}` not found."
         end
@@ -63,7 +72,7 @@ module Shopware
       def categories
         client = API::Client.new options.api
 
-        categories = client.categories
+        categories = client.get_categories
 
         categories.each_with_index do |category, i|
           id             = category['id']
