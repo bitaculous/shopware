9648a0d7f57eda96c0f8e6767627ee24341ebf10
Maik Kempe <mkempe@nrolla.com>
2014-05-23 19:23:48 +0200

    [➠] DAO™.

--- lib/shopware/cli/tasks/import.rb
+++ lib/shopware/cli/tasks/import.rb
@@ -18,36 +18,38 @@ module Shopware
                 col_sep    = options.import['column_separator'] || '|'
                 quote_char = options.import['quote_character'] || '"'
 
-                data = CSV.read file, col_sep: col_sep , quote_char: quote_char
+                data     = CSV.read file, col_sep: col_sep , quote_char: quote_char
                 quantity = data.length
 
                 data.each_with_index do |row, i|
                   info "Processing #{i + 1} of #{quantity} entries..." if options[:verbose]
 
-                  category         = row[0]
-                  subcategory      = row[1]
-                  subcategory_text = row[2]
-                  subsubcategory   = row[3]
-                  brand            = row[4]
-                  number           = row[5]
-                  name             = row[6]
-                  content          = row[7]
-                  content_unit     = row[8]
-                  order_number     = row[9]
-                  description      = row[10]
-                  code             = row[11]
-                  car_manufacturer = row[12]
-                  car_id           = row[13]
-                  property         = row[14]
-                  test_method      = row[15]
-                  property_value   = row[16]
-                  property_unit    = row[17]
-                  image_small      = row[18]
-                  image_big        = row[19]
+                  dao = OpenStruct.new(
+                    category:         row[0],
+                    subcategory:      row[1],
+                    subcategory_text: row[2],
+                    subsubcategory:   row[3],
+                    brand:            row[4],
+                    number:           row[5],
+                    name:             row[6],
+                    content:          row[7],
+                    content_unit:     row[8],
+                    order_number:     row[9],
+                    description:      row[10],
+                    code:             row[11],
+                    car_manufacturer: row[12],
+                    car_id:           row[13],
+                    property:         row[14],
+                    test_method:      row[15],
+                    property_value:   row[16],
+                    property_unit:    row[17],
+                    image_small:      row[18],
+                    image_big:        row[19]
+                  )
 
-                  category       = find_or_create_category name: category, template: options[:category_template], parent_id: options[:products_category_id]
-                  subcategory    = find_or_create_category name: subcategory, text: subcategory_text, template: options[:category_template], parent_id: category['id']
-                  subsubcategory = find_or_create_category name: subsubcategory, template: options[:category_template], parent_id: subcategory['id']
+                  category       = find_or_create_category name: dao.category, template: options[:category_template], parent_id: options[:products_category_id]
+                  subcategory    = find_or_create_category name: dao.subcategory, text: dao.subcategory_text, template: options[:category_template], parent_id: category['id']
+                  subsubcategory = find_or_create_category name: dao.subsubcategory, template: options[:category_template], parent_id: subcategory['id']
                 end
               else
                 error "File: `#{file}` not found."
@@ -69,7 +71,7 @@ module Shopware
               parentId: parent_id
             }
 
-            properties[:cmsText] = text if text
+            properties[:cmsText] = text if text and not text.empty?
 
             category = @client.create_category properties
 
