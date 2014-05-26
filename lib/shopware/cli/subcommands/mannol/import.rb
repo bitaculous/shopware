367fbbbbc27c84984a371ca33d3a405a9e44cb47
Maik Kempe <mkempe@nrolla.com>
2014-05-26 20:02:57 +0200

    [➦] Merged changes.

--- lib/shopware/cli/subcommands/mannol/import.rb
+++ lib/shopware/cli/subcommands/mannol/import.rb
@@ -86,6 +86,8 @@ module Shopware
                       end
                     end
                   end
+
+                  ok 'Import finished.'
                 else
                   error "File: `#{file}` not found." if options.verbose?
                 end
